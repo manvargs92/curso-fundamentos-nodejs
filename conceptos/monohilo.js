d4d98655b3a063a139b06d5296fcd417b64b1e4f
manvargs92 <manuel.vargas.garrido@outlook.com>
2022-06-07 13:45:42 -0500

    Monohilo: implicaciones endiseño y seguridad

---
+++ conceptos/monohilo.js
@@ -0,0 +1,15 @@
+console.log("Hola Mundo!!");
+
+let i = 0;
+setInterval(function () {
+    // console.log("Sigo activo...");
+    console.log(i);
+    i++;
+
+    // if (i === 5) {
+    //     console.log("Forzamos error...");
+    //     var a = 3 + z;
+    // }
+}, 1000); // ejecuta el codigo cada 1 seg.
+
+console.log("Segunda instrucción");
